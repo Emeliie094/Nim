d25819db79872991f3dd47a45841e31123e73633
Anna Blankell <anna.blankell@outlook.com>
2020-12-10 19:41:27 +0100

    added cmts and disabled players from start

--- script.js
+++ script.js
@@ -3,73 +3,98 @@ document.addEventListener("DOMContentLoaded", function () {
     constructor(player_in_turn, stack) {
       this.player_in_turn = player_in_turn;
       this.stack = stack;
-      this.playersarray = [];
+      this.playersArray = [];
     }
+
+    //Skapat klassen Game med följande key-value pairs i constuctorn.
 
     new_game() {
       let player1Name = prompt("Please enter your name player 1");
       let player2Name = prompt("Please enter your name player 2");
+      //När vi kallar på metoden new_game(), dyker en prompt-ruta upp där vi ber spelarna att skriva in namn.
 
-      //console.log(this.player);
       document.getElementById("player1Name").innerHTML = player1Name;
       document.getElementById("player2Name").innerHTML = player2Name;
+      //Sätter namnet på respektive spelare i en span i html-filen
       let player1 = new Player(player1Name);
       let player2 = new Player(player2Name);
+      //Skapar nya objekt av klassen Player och tilldelar dem varsin variabel. Skickar med namnen i parametrarna.
 
-      this.playersarray.push(player1);
-      this.playersarray.push(player2);
+      this.playersArray.push(player1);
+      this.playersArray.push(player2);
+      //Pushar in spelarna i arrayen players
 
       this.stack = "IIIIIIIIIIIIIIIIIIII";
+      //Skapar vår stack, som består av 21 st 'i'
       document.getElementById("stack").innerHTML = this.stack;
+      //En span i html-filen med id 'stack' tilldelas nu värdet av vår stack.
       this.slump_player();
+      //tillkallar metoden slumpplayer()
       console.log(player1, player2);
     }
     slump_player() {
       let randomNr = Math.floor(Math.random() * 2) + 1;
+
       if (randomNr === 1) {
         document.getElementById("btn_player2").disabled = true;
         let element = document.getElementById("h_player1");
+        document.getElementById("noPl2").disabled = true;
+        document.getElementById("noPl2").value = "";
+        document.getElementById("h_player2").style.color = "black";
+
         element.style.color = "green";
       } else {
         document.getElementById("btn_player1").disabled = true;
         let element = document.getElementById("h_player2");
+        document.getElementById("noPl1").disabled = true;
+        document.getElementById("noPl1").value = "";
+        document.getElementById("h_player1").style.color = "black";
+
         element.style.color = "green";
-        //NOTE: //DISABLA SPELARE
+        /*-------LA TILL SÅ ATT DEN SOM EJ SLUMPAS FRAM, BLIR DISABLED REDAN FRÅN BÖRJAN. TIDIGARE VAR BARA KNAPPEN DISABLED */
+
+        //Slumpar fram vilken spelare som ska börja. Den som INTE börjas, ska få en disabled knapp och number-input-.
+        //Den som börjar ska få rubriken i grönt.
       }
     }
 
-    //Har ändrat här i noOfSticks!! När man tar den sista pinnen så poppar en confirm ruta upp och man får välja om man vill spela igen eller bara avbryta. Om man vill spela igen så startar ett nytt spel.
-
     noOfSticks(e) {
-      /* NY RAD*/ if (this.stack.length > 1) {
+      if (this.stack.length > 1) {
         this.stack = this.stack.slice(0, -e);
         console.log(this.stack);
         document.getElementById("stack").innerHTML = this.stack;
       } else {
         this.endGame();
       }
+
+      //Metoden noOfSticks har en if-sats som säger att om vår stack har mer än 1 värde, så ska vi slice:a bort det antal (e) som vi har skrivit in i vår number-input. Är värdet mindre än 1, så avslutas spelet.
     }
 
     checkSum() {
       if (this.stack.length < 1) {
         this.endGame();
       }
+
+      //Detta är en metod för att checka så att stacken är mindre än 1.
     }
     endGame() {
       confirm("Du förlorade, vill ni spela igen?");
-      console.log(this.playersarray);
+      console.log(this.playersArray);
 
       // if (game_over === true) {
       if ((document.getElementById("btn_player1").disabled = true)) {
-        let winner = this.playersarray[0];
+        let winner = this.playersArray[0];
         winner.addPoints();
         highScore.push(winner);
       } else {
-        let winner = this.playersarray[1];
+        let winner = this.playersArray[1];
         winner.addPoints();
         highScore.push(winner);
         console.log(highScore);
       }
+
+      //Metoden endgame() kollar vilken spelare som var disabled när spelet slutade. Om spelare var disabled betyder det att hen vann.
+      //Vi kallar på metoden addpoints(). Sedan pushar vi in vår winner i arrayen highScore.
     }
   }
 
@@ -81,6 +106,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     addPoints() {
       this.score += 2;
+      //Metoden addPoints() lägger till två poäng till vinnaren.
     }
   }
 
@@ -89,6 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("startGame").addEventListener("click", function () {
     newGame.new_game();
+    //När vi klickar på Start game så startas ett nytt spel.
   });
 
   let btplayer1 = document.getElementById("btn_player1");
@@ -108,6 +135,10 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("btn_player2").disabled = false;
     document.getElementById("noPl2").disabled = false;
     document.getElementById("h_player2").style.color = "green";
+
+    //När spelare 1 klickar på knappen 1 att ta pinnar händer följande:
+    //Value blir det value som spelaren skrivit in i number-fältet. Detta skickas in i metoden noOfSticks.
+    //Spelare 2 blir disabled
   });
 
   btplayer2.addEventListener("click", function () {
@@ -123,4 +154,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("noPl1").disabled = false;
     document.getElementById("h_player1").style.color = "green";
   });
+
+  //När spelare 2 klickar på knappen 1 att ta pinnar händer följande:
+  //Value blir det value som spelaren skrivit in i number-fältet. Detta skickas in i metoden noOfSticks.
+  //Spelare 1 blir disabled
 });
